adbd0bbb3bee0f40f5f87bfa4c27af2e04c6b463
justin-cheng <justin-cheng@163.com>
2023-11-03 12:37:10 +0800

    Environment constructionâ

---
+++ src/.vuepress/sidebar.js
@@ -0,0 +1,11 @@
+import { sidebar } from "vuepress-theme-hope";
+
+export default sidebar({
+  "/guide/linux/":[
+    {
+      text: "git学习",
+      prefix: "/linux/",
+      collapsible: true,
+    },
+  ]
+});
